d5de35df01bd05ad6723e61d1e353204c1a4c3db
Alex Dykstra <adykstra1406@gmail.com>
2014-02-11 12:24:41 -0500

    modified Bird.java

--- Bird.java
+++ Bird.java
@@ -4,11 +4,21 @@ public class Bird {
     public int y;
     public int width;
     public int height;
+    public int gravity;
 
     public Bird () {
         x = 100;
         y = 300;
         width = 40;
         height = 40;
+    }
+
+    //method that causes bird to fall
+    public void gravity{
+        gravity = -2;
+
+        while (y >= 0){
+            y += gravity;
+        }
     }
 }
