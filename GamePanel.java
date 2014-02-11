0c98d8150bb9d54fee792689f5409ac477e9a5c4
Grant Titus <grant@granttitus.com>
2014-02-10 22:01:33 -0800

    created a thread for game logic

--- GamePanel.java
+++ GamePanel.java
@@ -1,13 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
 
     private Bird bird;
 
     public GamePanel () {
         Game game = new Game();
         bird = game.bird;
+
+        new Thread(this).start();
+    }
+
+    public void update () {
+        repaint();
     }
 
     protected void paintComponent (Graphics g) {
@@ -15,5 +21,17 @@ public class GamePanel extends JPanel {
 
         g.setColor(Color.GREEN);
         g.fillRect(bird.x, bird.y, bird.width, bird.height);
+    }
+
+    public void run () {
+        try {
+            while (true) {
+                update();
+                Thread.sleep(25);
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
